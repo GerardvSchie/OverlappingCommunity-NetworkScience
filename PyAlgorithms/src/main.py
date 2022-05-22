362b1bd3d334458cb18580f29b6214b5b7e9f8cd
GerardvSchie <g.j.vanschie@students.uu.nl>
2022-05-22 17:35:00 +0200

    Small cleanup

--- PyAlgorithms/src/main.py
+++ PyAlgorithms/src/main.py
@@ -8,8 +8,6 @@ from argparse import Namespace
 
 LFR_BIN = os.path.abspath("../../LFR-Benchmark_UndirWeightOvp/x64/Debug")
 OSLOM_BIN = os.path.abspath("../../Oslom_visual_1_5_8/x64/Debug")
-# OSLOM -f example.dat -uw -time 0.005 -infomap 3 -copra 2 -louvain 1
-
 
 def run_demon(G):
     dm = d.Demon(graph=G, epsilon=0.25, min_community_size=3)
@@ -64,16 +62,6 @@ def read_graph(edges_path, weighted=False):
 
 
 def run_oslom(path, weighted=False):
-    # run OSLOM with files already on disk
-    # args = Namespace()
-    # args.edges = path
-    # args.output_clusters = os.path.abspath("../test/output.json")
-    # args.oslom_output = os.path.abspath("../test")
-    # args.min_cluster_size = oslom.DEF_MIN_CLUSTER_SIZE = 3
-    # args.oslom_exec = oslom.DEF_OSLOM_EXEC = os.path.abspath("../../Oslom_visual_1_5_8/x64/Debug/OSLOM.exe")
-    # args.oslom_args = oslom.DEF_OSLOM_ARGS = ["-w"]
-    # oslom.run(args)
-
     weightedArg = None
     if weighted:
         weightedArg = "-w"
@@ -90,7 +78,6 @@ def run_oslom(path, weighted=False):
         "-copra", "2",
         "-louvain", "1"
     ]
-
     subprocess.run(command, shell=True, check=True)
 
 # Press the green button in the gutter to run the script.
@@ -116,30 +103,3 @@ if __name__ == '__main__':
     #
     # # Visualize the network
     # visual_graph(G)
-
-# # run OSLOM with files already on disk
-# args = Namespace()
-# args.edges = "/path/to/input_edges.tsv"
-# args.output_clusters = "/path/to/output_clusters.json"
-# args.oslom_output = "C:/Users/gerard/test"
-# args.min_cluster_size = oslom.DEF_MIN_CLUSTER_SIZE
-# args.oslom_exec = oslom.DEF_OSLOM_EXEC
-# args.oslom_args = oslom.DEF_OSLOM_ARGS
-# oslom.run(args)
-
-# run OSLOM with data in Python objects
-# args = Namespace()
-# args.min_cluster_size = 0
-# args.oslom_exec = oslom.DEF_OSLOM_EXEC
-# args.oslom_exec = "C:/Users/gerard/MegaDrive/Documents/M1.4_Network_Science/GitOverlappingCommunities/Oslom_visual_1_5_8/x64/Debug/OSLOM.exe"
-# args.oslom_args = oslom.DEF_OSLOM_ARGS
-# args.oslom_output = "C:/Users/gerard/test"
-
-
-# edges is an iterable of tuples (source, target, weight)
-# in the same format as the command-line version
-# edges = [(0, 1, 1.0), (1, 2, 1), (2, 0, 1)]
-# clusters = oslom.run_in_memory(args, edges)
-# print(clusters)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
